032abfec05dd1757ffc636321118410c3e677027
eddie <931305033@qq.com>
2019-02-18 10:17:16 +0800

    增加Servlet两种请求方式的处理方法 && 子类处理器通过复写父类方法实现Put & Delete 请求方式

--- src/main/java/com/eddie/tomcat/provider/Servlet.java
+++ src/main/java/com/eddie/tomcat/provider/Servlet.java
@@ -10,7 +10,31 @@ import com.eddie.tomcat.handle.incloud.Response;
  */
 public abstract class Servlet {
 
+    /**
+     * 处理Get请求
+     * @param request
+     * @param response
+     */
     public abstract void doGet(Request request, Response response);
 
-    public abstract void doPost(Request request,Response response);
+    /**
+     * 处理Post请求
+     * @param request
+     * @param response
+     */
+    public abstract void doPost(Request request, Response response);
+
+    /**
+     * 处理Put请求
+     * @param request
+     * @param response
+     */
+    public void doPut(Request request, Response response){}
+
+    /**
+     * 处理Delete请求
+     * @param request
+     * @param response
+     */
+    public void doDelete(Request request, Response response){}
 }
